799532eba690e101673bcdd442844591aad2259e
Shoujin Huang <105270555+Solor-pikachu@users.noreply.github.com>
2024-04-23 21:28:17 +0800

    Update train.sh

--- nila_training/train.sh
+++ nila_training/train.sh
@@ -1,5 +1,5 @@
 MODEL_FLAGS="--image_size 256 --num_channels 128 --num_res_blocks 2 --num_heads 4 --learn_sigma True"
 DIFFUSION_FLAGS="--diffusion_steps 1000 --noise_schedule linear"
-TRAIN_FLAGS="--lr 1e-4 --batch_size 8"
+TRAIN_FLAGS="--use_fp16 True --lr 1e-4 --batch_size 8"
 
 mpiexec -n 8 python scripts/image_train.py --data_dir data/fastmri_complex_value/ --log_dir log $MODEL_FLAGS $DIFFUSION_FLAGS $TRAIN_FLAGS
